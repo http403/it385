81e7588afbb277170b18650a5e645892fe58a5d3
Hartman Tam <hartmantam@pm.me>
2021-05-16 03:44:10 -0700

    first trial

---
+++ ansible/week6_activity.sh
@@ -0,0 +1,57 @@
+#!/bin/env bash
+
+# Check root https://askubuntu.com/a/30157
+if ! [ $(id -u) = 0 ]; then
+    echo "This script must run under root!"
+    exit 1
+fi
+
+# Apt update and install Ansible
+apt-get -q update && apt-get -qy install ansible sshpass
+
+# Write inventory.txt https://linuxize.com/post/bash-write-to-file/#writing-to-a-file-using-redirection-operators
+cat << EOF > inventory.txt
+[all]
+csr1    ansible_host=192.168.0.11
+csr2    ansbile_host=192.168.0.12
+web1    ansible_host=192.168.0.111
+web2    ansible_host=192.169.0.112
+db1     ansible_host=192.169.0.121
+db2     ansible_host=192.169.0.122
+
+[all:vars]
+ansible_ssh_extra_args='-o StrictHostKeyChecking=no'
+
+[csr]
+csr1
+csr2
+
+[linux]
+web1
+web2
+db1
+db2
+
+[web]
+web1
+web2
+
+[db]
+db1
+db2
+EOF
+
+# Testing ad-hoc commans 
+echo Pinging all Linux machines
+ansible -i inventory.txt linux -m ping -k
+
+# Upgrade to use SSH public key and save me the headache of typing password
+echo SSH Public Key!!!
+ssh-keygen -q -t ed25519 -f ansible -N "" -C ""     # Generate one for ansible only
+ssh-agent bash      # Start up ssh-agent
+ssh-add ./ansible   # Add key to ssh-agent
+SSH_PUBLIC_KEY=$(<cat ansible.pub)      # Load SSH public key to env var
+ansible -i inventory.txt linux -m authorized_key -a "key=$SSH_PUBLIC_KEY" -k   # Upload key via Ansible
+
+# Login again to see if password needed
+ansible -i inventory.txt linux -m ping 
